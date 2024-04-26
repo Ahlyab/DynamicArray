ca96206b931a99841d90c1e15670e9f5ae06eb4e
Ahlyab <ahlyabasad@gmail.com>
2024-04-26 19:06:35 +0500

    Enums

--- DynamicArray/DynamicArray.cpp
+++ DynamicArray/DynamicArray.cpp
@@ -1,110 +1,34 @@
 #include <iostream>
+#include <chrono>
+#include <thread>
 using namespace std;
 
-// Todo: overload [] operator, add find method that returns the index if it finds the element in the array otherwise return -1
-class DynamicArray {
-private:
-    int* arr;
-    int size;
-    int length;
-    int index;
-public:
-    DynamicArray() {
-        size = 5;
-        arr = new int[size];
-        length = 0;
-        index = 0;
-    }
+enum TrafficLights {red=10, yellow= 15, green = 18 };
 
-    int getLength() const {
-        return length;
-    }
-
-    int getCapacity() const {
-        return size;
-    }
-
-    void insert(int value) {
-        // if array is already filled
-        if (length == size) {
-            size *= 2;
-            int* ptr = new int[size];
-
-            // copying values
-            for (int i = 0; i < length; ++i) {
-                ptr[i] = arr[i];
-            }
-
-            delete[] arr;
-            arr = ptr;
-        }
-
-        arr[index] = value;
-        ++index;
-        ++length;
-    }
-
-    ~DynamicArray() {
-        delete[] arr;
-    }
-
-    void print() {
-        for (int i = 0; i < length; ++i) {
-            cout << arr[i] << " ";
-        }
-        cout << endl;
-    }
-
-    int getAt(int i) {
-        if (i < length && i >= 0) {
-            return arr[i];
-        }
-        return INT_MIN;
-    }
-
-    bool search(int target) {
-        for (int i = 0; i < length; ++i) {
-            if (arr[i] == target) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    void sort() {
-        for (int i = 0; i < length; ++i) {
-            for (int j = i+1; j < length; ++j) {
-                if (arr[i] > arr[j]) {
-                    int temp = arr[i];
-                    arr[i] = arr[j];
-                    arr[j] = temp;
-                }
-            }
-        }
-    }
-};
 int main()
 {
-    DynamicArray arr;
-    cout << "Size : " << arr.getLength() << "\nCapacity : " << arr.getCapacity() << endl;
+    TrafficLights trafficLight = red;
 
-    for (int i = 10; i > 0; --i) {
-        arr.insert(i);
+    while (true) {
+        if (trafficLight == red) {
+            cout << "Red: Please Stop" << endl;
+        }
+        this_thread::sleep_for(chrono::milliseconds(5000)); // 5 sec
+        trafficLight = yellow;
+
+        if (trafficLight == yellow) {
+            cout << "Yellow : Start Engine" << endl;
+        }
+
+        this_thread::sleep_for(chrono::milliseconds(3000)); // 3 sec
+        trafficLight = green;
+
+        if (trafficLight == green) {
+            cout << "Green : You are good to Go" << endl;
+        }
+
+        this_thread::sleep_for(chrono::milliseconds(5000)); // 5 sec
+        trafficLight = red;
     }
-
-    arr.print();
-    cout << "Size : " << arr.getLength() << "\nCapacity : " << arr.getCapacity() << endl;
-
-    arr.insert(6);
-
-    arr.print();
-    cout << "Size : " << arr.getLength() << "\nCapacity : " << arr.getCapacity() << endl;
-
-    cout << arr.getAt(8) << endl; // 5
-
-    cout << fixed << boolalpha << arr.search(5) << endl;
-
-    arr.sort();
-    arr.print();
 
 }
