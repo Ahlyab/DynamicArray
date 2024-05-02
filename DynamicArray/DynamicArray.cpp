d1ed4973f614697fc287e40d4700f7bb82a8b511
Ahlyab <ahlyabasad@gmail.com>
2024-05-02 18:22:40 +0500

    Example

--- DynamicArray/DynamicArray.cpp
+++ DynamicArray/DynamicArray.cpp
@@ -3,26 +3,35 @@
 #include <set>
 using namespace std;
 
+class divideZeroException : public std::exception
+{
+public:
+    const char* what() const noexcept override
+    {
+        return "Denominator is ZERO";
+    }
+};
 
 int main()
 {
-    vector<int> arr = { 1,1,2,3,3,4,5,5,6,7,8,7,7,6,5,55,4,4,3,23,213,2,3,4,1,3,5,6,7,8,9,5,4,3,2,1,1,1,1 };
-    set<int> s;
-
-    for (int n : arr) {
-        s.insert(n);
+    vector<int> arr = { 1, 2, 3, 4, 5 };
+    int a = 0, b = 0;
+    try
+    {
+        if (b == 0)
+        {
+            throw divideZeroException();
+        }
+        else
+        {
+            cout << a / b << endl;
+        }
     }
-
-    arr.clear();
-
-    for (int n : s) {
-        arr.push_back(n);
+    catch (const exception& e)
+    {
+        cerr << e.what() << '\n';
+        // cout << "Idr araha ho" << endl;
     }
-
-    for (int n : arr) {
-        cout << n << " ";
-    }
-
 }
 
 /*
